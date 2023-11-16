9247141cf1c6e0014295cf9fe4017fd43f50c744
Marco Antonio <marco.asousa09@gmail.com>
2023-11-15 21:59:50 -0300

    commitando mudanças na pagina de oferta

--- Lux/src/main/webapp/Modelos/script.js
+++ Lux/src/main/webapp/Modelos/script.js
@@ -1,11 +1,11 @@
 const cardImages = document.querySelectorAll('.card-image');
 
+image.addEventListener('mouseout', () => {
+  image.classList.remove('zoomed');
+});
+
 cardImages.forEach((image) => {
   image.addEventListener('mouseover', () => {
     image.classList.add('zoomed');
-  });
-
-  image.addEventListener('mouseout', () => {
-    image.classList.remove('zoomed');
   });
 });
