63b24d032e096b563154891dadd5ba60dc0a4383
Willian de Almeida Oliveira <wlln.almeida.oliveira@gmail.com>
2023-11-15 22:51:36 -0300

    Subindo a parte de admin do projeto

---
+++ Lux/src/main/java/br/com/lux/servlet/DetalhesCarrosAdmin.java
@@ -0,0 +1,22 @@
+package br.com.lux.servlet;
+import br.com.lux.dao.CarDao;
+import br.com.lux.model.Car;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+    @WebServlet("/admin-detalhes-admin")
+    public class DetalhesCarrosAdmin extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+            
+            String carId = req.getParameter("id");
+            Car car = new CarDao().findCarById(carId).get(0);
+            req.setAttribute("car", car);
+            req.getRequestDispatcher("admin.jsp").forward(req, resp);
+        }
+}
